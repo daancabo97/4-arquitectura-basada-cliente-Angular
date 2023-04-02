f62b5da6171d2d0cb240ab662400649af763c46c
daancabo97 <danielcaicedo285@gmail.com>
2023-04-02 13:55:00 -0500

    crud-documentos

---
+++ src/enviroment/enviroment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyB_fQFWbXmMTkUiSKt9m1NPB7srvsFmCHA",
+        authDomain: "documentos-crud.firebaseapp.com",
+        projectId: "documentos-crud",
+        storageBucket: "documentos-crud.appspot.com",
+        messagingSenderId: "941444776497",
+        appId: "1:941444776497:web:65b5082e23456cf36d4b10"
+    }
+  };
